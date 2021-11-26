e85b234d18ffc10f4c2a3a3d78ef432b7801ecb6
Théo Billardey <theo.billardey@gmail.com>
2021-11-26 11:44:37 +0100

    update User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,25 @@
-interface User {
-  id: string;
-  profilePicture: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-}
+import "reflect-metadata";
+import { ObjectType, Field, ID } from "type-graphql";
+import { IsEmail } from "class-validator";
 
-export default User;
+@ObjectType()
+export class User {
+  @Field(() => ID)
+  id!: number;
+
+  @Field(() => String)
+  profilePicture?: string;
+
+  @Field(() => String)
+  firstName?: string;
+
+  @Field(() => String)
+  lastName?: string;
+
+  @Field()
+  @IsEmail()
+  email!: string;
+
+  @Field(() => String)
+  password!: string;
+}
