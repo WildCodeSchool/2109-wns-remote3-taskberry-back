797b7e86015619233231d0c44997d112c6b7346a
Théo Billardey <toto.billardey@gmail.com>
2021-12-05 17:51:51 +0100

    add header token in context

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import * as tq from "type-graphql";
 import { UserResolver } from "./resolvers/UserResolver";
 import { ApolloServer } from "apollo-server";
 import { DateTimeResolver } from "graphql-scalars";
-import { context } from "./context";
 import { GraphQLScalarType } from "graphql";
 
 const app = async () => {
@@ -12,7 +11,14 @@ const app = async () => {
     scalarsMap: [{ type: GraphQLScalarType, scalar: DateTimeResolver }],
   });
 
-  new ApolloServer({ schema, context: context }).listen({ port: 4000 }, () =>
+  new ApolloServer({
+    schema,
+    context: ({ req }: { req: any }) => {
+      const token = req.headers.authorization;
+
+      return { token: token };
+    },
+  }).listen({ port: 4000 }, () =>
     console.log(`
 🚀 Server ready at: http://localhost:4000`)
   );
