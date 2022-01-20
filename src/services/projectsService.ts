48f18a74bfe3e54a2e11aacf83cfa119089225fa
brdtheo <theo.billardey@gmail.com>
2022-01-20 12:28:50 +0100

    add project resolver layers

---
+++ src/services/projectsService.ts
@@ -0,0 +1,41 @@
+import { Project } from "../models/Project";
+import projectsRepository from "../repositories/projectsRepository";
+import { ProjectInput } from "../inputs/ProjectInput";
+import { ProjectMemberInput } from "../inputs/ProjectMemberInput";
+import { UsersProject } from "../models/UsersProject";
+
+const projectService = {
+  create: (projectInput: ProjectInput): Promise<Project> => {
+    const { name } = projectInput;
+
+    if (!name) {
+      throw new Error("Name is required");
+    }
+
+    if (name.length > 30) {
+      throw new Error("Name should have at least one character and max 30");
+    }
+
+    return projectsRepository.create(projectInput);
+  },
+
+  addProjectMember: (memberInput: ProjectMemberInput): Promise<UsersProject> => {
+    const { roleId, userId, projectId } = memberInput;
+
+    if (!roleId) {
+      throw new Error("Role ID is required");
+    }
+
+    if (!userId) {
+      throw new Error("User ID is required");
+    }
+
+    if (!projectId) {
+      throw new Error("Project ID is required");
+    }
+
+    return projectsRepository.addProjectMember(memberInput);
+  },
+};
+
+export default projectService;
