bca45e35250a473cec8ab2c2fb2297e763a7e42c
brdtheo <theo.billardey@gmail.com>
2022-01-22 18:29:02 +0100

    add ticket types

---
+++ src/models/Ticket.ts
@@ -0,0 +1,28 @@
+import { ObjectType, Field, ID, Int } from "type-graphql";
+
+@ObjectType()
+export class Ticket {
+  @Field(() => ID)
+  id!: number;
+
+  @Field()
+  name!: string;
+
+  @Field(() => String, { nullable: true })
+  description!: string | null;
+
+  @Field(() => Date)
+  createdAt!: Date;
+
+  @Field(() => Date)
+  finishedAt!: Date | null;
+
+  @Field(() => Int)
+  projectId!: number;
+
+  @Field(() => Int)
+  statusId!: number;
+
+  @Field(() => Int)
+  assigneeId!: number;
+}
