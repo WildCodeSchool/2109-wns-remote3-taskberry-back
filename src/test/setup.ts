ff106d45eddc35762564b4f438a60db1de4376de
brdtheo <theo.billardey@gmail.com>
2022-01-23 17:54:58 +0100

    create tests setup

---
+++ src/test/setup.ts
@@ -0,0 +1,4 @@
+// import {Connection, createConnection} from "typeorm";
+// import {ApolloServer} from "apollo-server";
+// import Database from "better-sqlite3";
+// import createServer from "../server";
