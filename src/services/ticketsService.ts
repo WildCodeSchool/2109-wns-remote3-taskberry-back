800cac65ca4d373521dfce42f16d4766562f0667
Guy de Messimy <gdemessimy@gmail.com>
2022-02-22 11:07:25 +0100

    feat: get and update ticket

--- src/services/ticketsService.ts
+++ src/services/ticketsService.ts
@@ -1,10 +1,23 @@
 import { Ticket, PrismaClient } from "@prisma/client";
-import { TicketInput } from "../inputs/TicketInput";
+import { PartialUpdateTicketInput, TicketInput } from "../inputs/TicketInput";
 import ticketsRepository from "../repositories/ticketsRepository";
+const { UserInputError } = require("apollo-server");
 
 const prisma = new PrismaClient();
 
 const ticketService = {
+  getTicket: (ticketId: number): Promise<Ticket | null> => {
+    if (!ticketId) {
+      throw new Error("Ticket ID is required");
+    }
+    if (ticketId < 1) {
+      throw new UserInputError("Invalid argument value", {
+        argumentName: "id",
+      });
+    }
+    return ticketsRepository.getTicketById(ticketId);
+  },
+
   create: (ticketInput: TicketInput): Promise<Ticket> => {
     const { name } = ticketInput;
 
@@ -19,19 +32,9 @@ const ticketService = {
     return ticketsRepository.create(ticketInput);
   },
 
-  delete: (ticketId: number): Promise<Number> => {
-    if (!ticketId) {
-      throw new Error("Ticket ID is required");
-    }
-
-    if (typeof ticketId !== "number") {
-      throw new Error("Ticket ID must be a number");
-    }
-
-    return ticketsRepository.delete(ticketId);
-  },
-
-  getProjectTickets: async (projectId: number): Promise<Ticket[] | [] | null> => {
+  getProjectTickets: async (
+    projectId: number
+  ): Promise<Ticket[] | [] | null> => {
     const isProjectExists = await prisma.project.findUnique({
       where: { id: projectId },
     });
@@ -49,6 +52,27 @@ const ticketService = {
     }
 
     return ticketsRepository.getProjectTickets(projectId);
+  },
+
+  update: (partialInput: PartialUpdateTicketInput): Promise<Ticket> => {
+    const { id, name } = partialInput;
+
+    if (!id) {
+      throw new Error("TicketId is required");
+    }
+    return ticketsRepository.update(partialInput);
+  },
+
+  delete: (ticketId: number): Promise<Number> => {
+    if (!ticketId) {
+      throw new Error("Ticket ID is required");
+    }
+
+    if (typeof ticketId !== "number") {
+      throw new Error("Ticket ID must be a number");
+    }
+
+    return ticketsRepository.delete(ticketId);
   },
 };
 
