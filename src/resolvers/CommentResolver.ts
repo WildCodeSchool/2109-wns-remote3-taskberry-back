edcfb20d071141edc53623a27c23c3570eca6acb
brdtheo <theo.billardey@gmail.com>
2022-03-30 10:29:37 +0200

    create comment resolver

---
+++ src/resolvers/CommentResolver.ts
@@ -0,0 +1,32 @@
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
+import { Comment } from "../models/Comment";
+import {
+  CommentInput,
+  PartialUpdateCommentInput,
+} from "../inputs/CommentInput";
+import commentService from "../services/commentService";
+
+@Resolver((of) => Comment)
+export class CommentResolver {
+  @Query(() => [Comment])
+  async getTicketComments(@Arg("ticketId") ticketId: number) {
+    return await commentService.getTicketComments(ticketId);
+  }
+
+  @Mutation(() => Comment)
+  async createComment(@Arg("commentInput") commentInput: CommentInput) {
+    return await commentService.create(commentInput);
+  }
+
+  @Mutation(() => Comment)
+  async updateComment(
+    @Arg("partialInput") partialInput: PartialUpdateCommentInput
+  ) {
+    return await commentService.update(partialInput);
+  }
+
+  @Mutation(() => Number)
+  async deleteComment(@Arg("commentId") commentId: number) {
+    return await commentService.delete(commentId);
+  }
+}
