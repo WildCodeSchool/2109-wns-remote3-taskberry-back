324a0275595eb6b6acf1342c4b4682289d37daa6
brdtheo <theo.billardey@gmail.com>
2022-03-30 10:27:15 +0200

    add Comment prisma model

---
+++ prisma/migrations/20220329130534_comment/migration.sql
@@ -0,0 +1,35 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `createdAt` on the `Project` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `finishedAt` on the `Project` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `estimateEndAt` on the `Project` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `createdAt` on the `Ticket` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+  - You are about to alter the column `finishedAt` on the `Ticket` table. The data in that column could be lost. The data in that column will be cast from `Timestamp(0)` to `Timestamp`.
+
+*/
+-- AlterTable
+ALTER TABLE `Project` MODIFY `createdAt` TIMESTAMP NOT NULL,
+    MODIFY `finishedAt` TIMESTAMP NULL,
+    MODIFY `estimateEndAt` TIMESTAMP NULL;
+
+-- AlterTable
+ALTER TABLE `Ticket` MODIFY `createdAt` TIMESTAMP NOT NULL,
+    MODIFY `finishedAt` TIMESTAMP NULL;
+
+-- CreateTable
+CREATE TABLE `Comment` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `description` VARCHAR(255) NOT NULL,
+    `createdAt` TIMESTAMP NOT NULL,
+    `userId` INTEGER NOT NULL,
+    `ticketId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Comment` ADD CONSTRAINT `Comment_ticketId_fkey` FOREIGN KEY (`ticketId`) REFERENCES `Ticket`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
