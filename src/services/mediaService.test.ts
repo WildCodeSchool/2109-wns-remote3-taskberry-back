ba72d3553a8ee148657beb95585719b1247fff72
brdtheo <theo.billardey@gmail.com>
2022-04-28 17:34:15 +0200

    create media service

---
+++ src/services/mediaService.test.ts
@@ -0,0 +1,192 @@
+import { PrismaClient } from "@prisma/client";
+import createUserAction from "../actions/createUserAction";
+import createProjectAction from "../actions/createProjectAction";
+import createStatusAction from "../actions/createStatusAction";
+import ticketService from "./ticketsService";
+import createTicketAction from "../actions/createTicketAction";
+import createMediaAction from "../actions/createMediaAction";
+import mediaService from "./mediaService";
+const faker = require("@faker-js/faker");
+
+const prisma = new PrismaClient();
+
+afterAll(() => {
+  return expect(prisma.$disconnect()).resolves;
+});
+
+describe("mediaService", () => {
+  it("creates new media correctly", async () => {
+    // create a user, role, project, status, ticket and media
+    const savedUser = await createUserAction({
+      prisma,
+      profilePicture: faker.image.people(500, 500),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    });
+
+    const savedProject = await createProjectAction({
+      prisma,
+      name: faker.internet.domainName(),
+      description: faker.random.words(10),
+      createdAt: faker.date.recent(),
+      estimateEndAt: faker.date.future(),
+    });
+
+    const savedStatus = await createStatusAction({
+      prisma,
+      name: faker.random.word(),
+    });
+
+    const name = faker.git.commitMessage();
+    const description = faker.random.words(10);
+    const createdAt = faker.date.recent();
+
+    const savedTicket = await ticketService.create({
+      name,
+      description,
+      projectId: savedProject.id,
+      statusId: savedStatus.id,
+      assigneeId: savedUser.id,
+      createdAt,
+    });
+
+    const mediaName = "test.jpg";
+    const mediaType = "image/jpeg";
+    const mediaUrl = faker.image.imageUrl();
+
+    const savedMedia = await mediaService.create({
+      name: mediaName,
+      type: mediaType,
+      url: mediaUrl,
+      createdAt: createdAt,
+      ticketId: savedTicket.id,
+    });
+
+    expect(savedMedia.name).toBe(mediaName);
+    expect(savedMedia.type).toBe(mediaType);
+    expect(savedMedia.url).toBe(mediaUrl);
+    expect(savedMedia.ticketId).toBe(savedTicket.id);
+  });
+
+  it("delete a media correctly", async () => {
+    const savedUser = await createUserAction({
+      prisma,
+      profilePicture: faker.image.people(500, 500),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    });
+
+    const savedProject = await createProjectAction({
+      prisma,
+      name: faker.internet.domainName(),
+      description: faker.random.words(10),
+      createdAt: faker.date.recent(),
+      estimateEndAt: faker.date.future(),
+    });
+
+    const savedStatus = await createStatusAction({
+      prisma,
+      name: faker.random.word(),
+    });
+
+    const name = faker.git.commitMessage();
+    const description = faker.random.words(10);
+    const createdAt = faker.date.recent();
+
+    const savedTicket = await createTicketAction({
+      prisma,
+      name,
+      description,
+      projectId: savedProject.id,
+      statusId: savedStatus.id,
+      assigneeId: savedUser.id,
+      createdAt,
+    });
+
+    const mediaName = "test.jpg";
+    const mediaType = "image/jpeg";
+    const mediaUrl = faker.image.imageUrl();
+
+    const savedMedia = await mediaService.create({
+      name: mediaName,
+      type: mediaType,
+      url: mediaUrl,
+      createdAt: createdAt,
+      ticketId: savedTicket.id,
+    });
+
+    await mediaService.delete(savedMedia.id);
+
+    const deletedMedia = await prisma.media.findUnique({
+      where: { id: savedMedia.id },
+    });
+
+    expect(deletedMedia).toBeNull();
+  });
+
+  it("get ticket media correctly", async () => {
+    const savedUser = await createUserAction({
+      prisma,
+      profilePicture: faker.image.people(500, 500),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    });
+
+    const savedProject = await createProjectAction({
+      prisma,
+      name: faker.internet.domainName(),
+      description: faker.random.words(10),
+      createdAt: faker.date.recent(),
+      estimateEndAt: faker.date.future(),
+    });
+
+    const savedStatus = await createStatusAction({
+      prisma,
+      name: faker.random.word(),
+    });
+
+    const name = faker.git.commitMessage();
+    const description = faker.random.words(10);
+    const createdAt = faker.date.recent();
+
+    const savedTicket = await createTicketAction({
+      prisma,
+      name,
+      description,
+      projectId: savedProject.id,
+      statusId: savedStatus.id,
+      assigneeId: savedUser.id,
+      createdAt,
+    });
+
+    await createMediaAction({
+      prisma,
+      name: "test.jpg",
+      type: "image/jpeg",
+      url: faker.image.imageUrl(),
+      createdAt: createdAt,
+      ticketId: savedTicket.id,
+    });
+    await createMediaAction({
+        prisma,
+        name: "test2.jpg",
+        type: "image/jpeg",
+        url: faker.image.imageUrl(),
+        createdAt: createdAt,
+        ticketId: savedTicket.id,
+      });
+
+    const ticketMedia = await mediaService.getTicketMedia(
+      savedTicket.id
+    );
+
+    expect(ticketMedia).toBeTruthy();
+    expect(ticketMedia).toHaveLength(2);
+  });
+});
